289d326e17f3c1fbcc6f8c8898523ae05ad2598c
radotooo <radotooo@gmail.com>
2021-01-30 13:03:40 +0200

    init

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,15 +1,18 @@
-import { Sprite } from 'pixi.js';
 import Scene from './Scene';
-import gsap from 'gsap';
 import Footer from '../components/Footer';
+import Fish from '../components/Fish';
+import { Texture } from 'pixi.js';
 
 export default class Play extends Scene {
   async onCreated() {
-
     const footer = new Footer();
-    footer.x = - window.innerWidth / 2;
+    footer.x = -window.innerWidth / 2;
     footer.y = window.innerHeight / 2 - footer.height;
-    this.addChild(footer);
+    // this.addChild(footer);
+
+    const texture = Texture.from('smallFish');
+    const fish = new Fish(texture);
+    this.addChild(fish);
   }
 
   /**
@@ -19,7 +22,7 @@ export default class Play extends Scene {
    * @param  {Number} width  Window width
    * @param  {Number} height Window height
    */
-  onResize(width, height) { // eslint-disable-line no-unused-vars
-
+  onResize(width, height) {
+    // eslint-disable-line no-unused-vars
   }
 }
